25854eefec237a2ab71bd7db0dd5d9776ceef005
hennyg2001 <henrikmiki@gmail.com>
2022-05-19 13:09:52 +0200

    Connecting repository to view models 4

--- app/src/main/java/com/example/greenhouseappnew/ui/plant_profile/PlantProfileViewModel.java
+++ app/src/main/java/com/example/greenhouseappnew/ui/plant_profile/PlantProfileViewModel.java
@@ -7,6 +7,7 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
 import com.example.greenhouseappnew.model.Routine;
+import com.example.greenhouseappnew.network.GreenhouseRepository;
 
 import java.util.List;
 
@@ -16,27 +17,27 @@ public class PlantProfileViewModel extends AndroidViewModel {
         super(application);
     }
 
-    private GreenhouseService service;
+    private GreenhouseRepository repository;
     private LiveData<List<Routine>> allRoutines;
 
     public LiveData<Routine> get(int id) {
-
+        return repository.getRoutine(id);
     }
 
     public LiveData<List<Routine>> getAll() {
-
+        return allRoutines;
     }
 
     public void insert(Routine routine) {
-
+        repository.addRoutine(routine);
     }
 
     public void update(Routine routine) {
-
+        repository.updateRoutine(routine);
     }
 
     public void delete(Routine routine) {
-
+        repository.deletRoutine(routine.getId());
     }
 
 }
