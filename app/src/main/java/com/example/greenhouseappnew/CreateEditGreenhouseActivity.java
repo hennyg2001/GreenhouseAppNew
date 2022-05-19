f2c9229374f30dbd764b69c7e1fd89fd6b127e55
hennyg2001 <henrikmiki@gmail.com>
2022-05-19 11:10:38 +0200

    Updated Greenhouse Adapter, Main and Greenhouses activity classes 2

--- app/src/main/java/com/example/greenhouseappnew/CreateEditGreenhouseActivity.java
+++ app/src/main/java/com/example/greenhouseappnew/CreateEditGreenhouseActivity.java
@@ -1,5 +1,8 @@
 package com.example.greenhouseappnew;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
@@ -10,12 +13,69 @@ public class CreateEditGreenhouseActivity extends AppCompatActivity {
 
     public static final String EXTRA_ID = "com.example.and_recipeapp.EXTRA_ID";
     public static final String EXTRA_NAME = "com.example.and_recipeapp.EXTRA_NAME";
-    public static final String EXTRA_TYPE = "com.example.and_recipeapp.EXTRA_TYPE";
+    public static final String EXTRA_LOCATION = "com.example.and_recipeapp.EXTRA_LOCATION";
     public static final String EXTRA_DESCRIPTION = "com.example.and_recipeapp.EXTRA_DESCRIPTION";
 
-    private EditText nameEditText, typeEditText, descriptionEditText;
-    private Button createGreenhouseButton, cancelButton;
+    private EditText nameEditText, locationEditText, descriptionEditText;
+    private Button createGreenhouseButton;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_greenhouse);
 
+        nameEditText = findViewById(R.id.greenhouseName);
+        locationEditText = findViewById(R.id.greenhouseLocation);
+        descriptionEditText = findViewById(R.id.greenhouseDescription);
+        createGreenhouseButton = findViewById(R.id.createGreenhouseButton);
+
+        Intent intent = getIntent();
+
+        if(intent.hasExtra(EXTRA_ID)) {
+
+            setTitle("Edit Greenhouse");
+            nameEditText.setText(intent.getStringExtra(EXTRA_NAME));
+            locationEditText.setText(intent.getStringExtra(EXTRA_LOCATION));
+            descriptionEditText.setText(intent.getStringExtra(EXTRA_DESCRIPTION));
+
+        } else {
+
+            setTitle("Add Recipe");
+
+        }
+
+        createGreenhouseButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick (View v){
+
+                saveGreenhouse();
+
+            }
+
+        });
+
+    }
+
+    private void saveGreenhouse() {
+
+        String name = nameEditText.getText().toString().trim();
+        String location = locationEditText.getText().toString().trim();
+        String description = descriptionEditText.getText().toString().trim();
+
+        Intent data = new Intent();
+        data.putExtra(EXTRA_NAME, name);
+        data.putExtra(EXTRA_LOCATION, location);
+        data.putExtra(EXTRA_DESCRIPTION, description);
+
+        int id = getIntent().getIntExtra(EXTRA_ID, -1);
+        if(id != -1) {
+            data.putExtra(EXTRA_ID, id);
+        }
+
+        setResult(RESULT_OK, data);
+        finish();
+
+    }
 
 }
--- app/src/main/java/com/example/greenhouseappnew/GreenhouseActivity.java
+++ app/src/main/java/com/example/greenhouseappnew/GreenhouseActivity.java
@@ -1,6 +1,17 @@
 package com.example.greenhouseappnew;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.greenhouseappnew.model.Greenhouse;
 
 public class GreenhouseActivity extends AppCompatActivity {
 
@@ -9,6 +20,82 @@ public class GreenhouseActivity extends AppCompatActivity {
     public static final String GREENHOUSE_LOCATION = "com.example.and_recipeapp.GREENHOUSE_LOCATION";
     public static final String GREENHOUSE_DESCRIPTION = "com.example.and_recipeapp.GREENHOUSE_DESCRIPTION";
 
+    private TextView nameTextView, locationTextView, descriptionTextView;
+    private Button closeButton, editButton;
 
+    private GreenhousesViewModel greenhousesViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_greenhouse);
+
+        nameTextView = findViewById(R.id.greenhouseName);
+        locationTextView = findViewById(R.id.greenhouseLocation);
+        descriptionTextView = findViewById(R.id.greenhouseDescription);
+        editButton = findViewById(R.id.editGreenhouseButton);
+
+        Intent intent = getIntent();
+
+        nameTextView.setText(intent.getStringExtra(GREENHOUSE_NAME));
+        locationTextView.setText(intent.getStringExtra(GREENHOUSE_LOCATION));
+        descriptionTextView.setText(intent.getStringExtra(GREENHOUSE_DESCRIPTION));
+
+        // Launcher
+        ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+
+                    Intent data = result.getData();
+
+                    if(result.getResultCode() == RESULT_OK) {
+
+                        String name = data.getStringExtra(CreateEditGreenhouseActivity.EXTRA_NAME);
+                        String location = data.getStringExtra(CreateEditGreenhouseActivity.EXTRA_LOCATION);
+                        String description = data.getStringExtra(CreateEditGreenhouseActivity.EXTRA_DESCRIPTION);
+
+                        int id = data.getIntExtra(CreateEditGreenhouseActivity.EXTRA_ID, -1);
+
+                        if(id == -1) {
+                            Toast.makeText(this, "Recipe can't be updated...", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        Greenhouse greenhouse = new Greenhouse(name, location, description);
+                        greenhouse.setId(id);
+                        greenhousesViewModel.update(greenhouse);
+                        Toast.makeText(this, "Recipe updated...", Toast.LENGTH_SHORT).show();
+
+                    }
+                    else {
+
+                        Toast.makeText(this, "Recipe not saved...", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+        );
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v){
+
+                finish();
+
+            }
+        });
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v){
+
+                Intent it = new Intent(GreenhouseActivity.this, CreateEditGreenhouseActivity.class);
+                it.putExtra(CreateEditGreenhouseActivity.EXTRA_ID, intent.getStringExtra(GREENHOUSE_ID));
+                it.putExtra(CreateEditGreenhouseActivity.EXTRA_NAME, intent.getStringExtra(GREENHOUSE_NAME));
+                it.putExtra(CreateEditGreenhouseActivity.EXTRA_LOCATION, intent.getStringExtra(GREENHOUSE_LOCATION));
+                it.putExtra(CreateEditGreenhouseActivity.EXTRA_DESCRIPTION, intent.getStringExtra(GREENHOUSE_DESCRIPTION));
+                mStartForResult.launch(it);
+
+            }
+        });
 
 }
