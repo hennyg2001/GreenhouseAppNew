8e2f202ba04575d54e615bb23facf6356c576b44
jaime <jaime.elenatristan@gmail.com>
2022-05-20 10:58:17 +0200

    Fragment

--- app/src/main/java/com/example/greenhouseappnew/model/LogClass.java
+++ app/src/main/java/com/example/greenhouseappnew/model/LogClass.java
@@ -1,32 +1,40 @@
 package com.example.greenhouseappnew.model;
 
+import java.util.Date;
+
 public class LogClass {
-
     private int id;
-    private String name;
-    private String type;
+    private double co2;
+    private double temperature;
+    private double humidity;
+    private Date timeStamp;
 
-    public LogClass(String name, String type) {
-
-        this.name = name;
-        this.type = type;
-
-    }
-
-    public void setId(int id) {
+    public LogClass(int id, double co2, double temperature, double humidity, Date timeStamp)
+    {
         this.id = id;
+        this.co2 = co2;
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.timeStamp = timeStamp;
     }
 
     public int getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public double getCo2() {
+        return co2;
     }
 
-    public String getType() {
-        return type;
+    public double getTemperature() {
+        return temperature;
     }
 
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
 }
